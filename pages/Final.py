04b108aa5deeb1791ae463cb835950b66560888e
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2024-03-27 10:17:55 +0530

    Update Final.py

--- pages/Final.py
+++ pages/Final.py
@@ -3,8 +3,9 @@ from CompleteSystemProto import Generate
 import nltk
 
 def main():
-	with st.spinner("Generating Head Line!!!"):
-		st.success(Generate(st.session_state['Questions'][-1]))
+	if 'Questions' in st.session_state:
+		with st.spinner("Generating Head Line!!!"):
+			st.success(Generate(st.session_state['Questions'][-1]))
 
 if __name__ == "__main__":
 	main()
