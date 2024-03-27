390b29668ada9b059221d574a19f170962ea01dd
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2024-03-27 07:59:28 +0530

    Update BootLoader.py

--- WebComponents/BootLoader.py
+++ WebComponents/BootLoader.py
@@ -2,24 +2,31 @@ import requests
 import zipfile
 import os
 
+def Booting():
+	directory_path = "Parser"
+	
+	if os.path.exists(directory_path) and os.listdir(directory_path):
+		print("Parser is up and Running")
+	else:
+		ParserDownload()
+
 def ParserDownload():
 	url = "https://nlp.stanford.edu/software/stanford-corenlp-4.5.6.zip"
 	
 	filename = "stanford-corenlp-4.5.6.zip"
 	
-	directory = "./Parser/"
+	directory = "Parser"
 	
 	os.makedirs(directory, exist_ok=True)
 	
 	response = requests.get(url)
 	
 	if response.status_code == 200:
-	    with open(os.path.join(directory, filename), 'wb') as f:
-	        f.write(response.content)
-	    print("Download successful.")
-	
-	    with zipfile.ZipFile(os.path.join(directory, filename), 'r') as zip_ref:
-	        zip_ref.extractall(directory)
-	    print("Extraction successful.")
+		with open(os.path.join(directory, filename), 'wb') as f:
+			f.write(response.content)
+			print("Download successful.")
+		with zipfile.ZipFile(os.path.join(directory, filename), 'r') as zip_ref:
+			zip_ref.extractall(directory)
+			print("Extraction successful.")
 	else:
-	    print("Failed to download file.")
+		print("Failed to download file.")
