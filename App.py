0c8ab5568e8a6b640a374fc553d4d7de1f3aa282
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2024-03-27 09:19:17 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from WebComponents import BootLoader as bl
 
+st.set_page_config(layout="centered")
 def main():
 	with st.spinner("Checking for necessary Dependencies"):
 		bl.Booting()
