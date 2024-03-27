3d1b70be2c99f65058fbbdee813fea1965c005a4
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2024-03-27 09:28:02 +0530

    Update App.py

--- App.py
+++ App.py
@@ -8,8 +8,12 @@ def main():
 	with st.spinner("Checking for necessary Dependencies"):
 		bl.Booting()
 	k = os.listdir("Parser/stanford-corenlp-4.5.6")
+	flag = 0
 	for i in k:
-		st.write(i)
+		if i == "stanford-corenlp-4.5.6-models.jar" or i == "stanford-corenlp-4.5.6-sources.jar":
+			flag += 1
+	if flag == 2:
+		st.write("Found Parser Files")
 	if st.button("Upload Text"):
 		st.switch_page("pages/Extraction.py")
 if __name__ == "__main__":
