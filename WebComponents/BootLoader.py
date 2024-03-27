129cbc13584042ccae80306af8224f9ce6cefd5d
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2024-03-27 14:21:36 +0530

    Update BootLoader.py

--- WebComponents/BootLoader.py
+++ WebComponents/BootLoader.py
@@ -20,25 +20,27 @@ def Booting():
 def ParserDownload():
 	nltk.download('punkt')
 	nltk.download('stopwords')
-	url = "https://nlp.stanford.edu/software/stanford-corenlp-4.5.6.zip"
 	
-	filename = "stanford-corenlp-4.5.6.zip"
-	
-	directory = "Parser"
-	
-	os.makedirs(directory, exist_ok=True)
-	
-	response = requests.get(url)
-	
-	if response.status_code == 200:
-		with open(os.path.join(directory, filename), 'wb') as f:
-			f.write(response.content)
-			print("Download successful.")
-		with zipfile.ZipFile(os.path.join(directory, filename), 'r') as zip_ref:
-			zip_ref.extractall(directory)
-			print("Extraction successful.")
-	else:
-		print("Failed to download file.")
+	with st.spinner("Checking for necessary Dependencies"):
+		url = "https://nlp.stanford.edu/software/stanford-corenlp-4.5.6.zip"
+		
+		filename = "stanford-corenlp-4.5.6.zip"
+		
+		directory = "Parser"
+		
+		os.makedirs(directory, exist_ok=True)
+		
+		response = requests.get(url)
+		
+		if response.status_code == 200:
+			with open(os.path.join(directory, filename), 'wb') as f:
+				f.write(response.content)
+				print("Download successful.")
+			with zipfile.ZipFile(os.path.join(directory, filename), 'r') as zip_ref:
+				zip_ref.extractall(directory)
+				print("Extraction successful.")
+		else:
+			print("Failed to download file.")
 
 def Server():
 	st.session_state['server'] = CoreNLPServer()
