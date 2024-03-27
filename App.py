5cbb082b5d96b2ad3e84a26dae671271133ab090
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2024-03-27 14:13:31 +0530

    Update App.py

--- App.py
+++ App.py
@@ -5,11 +5,14 @@ import os
 import nltk
 
 def main():
+	st.session_state["Flag"] = True
 	os.environ['CLASSPATH'] = 'Parser/stanford-corenlp-4.5.6'
 	#os.environ['JAVAHOME'] = 'usr/bin/java'
 	st.title("Abstract Page")
 	with st.spinner("Checking for necessary Dependencies"):
-		bl.Booting()
+		if st.session_state["Flag"]:
+			bl.Booting()
+			st.session_state["Flag"] = False
 
 	if st.button("Upload Text"):
 		st.switch_page("pages/Extraction.py")
