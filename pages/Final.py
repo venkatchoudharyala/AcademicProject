220d7332b8e4e72f185828c80293c2f685b4081a
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2024-03-27 09:53:31 +0530

    Update Final.py

--- pages/Final.py
+++ pages/Final.py
@@ -2,7 +2,8 @@ import streamlit as st
 from CompleteSystemProto import Generate
 
 def main():
-  st.success(Generate(st.session_state['Question']))
+  with st.spinner("Generating Head Line!!!")
+  st.success(Generate(st.session_state['Questions'][-1]))
 
 if __name__ == __main__():
   main()
