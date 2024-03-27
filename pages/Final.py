a13e0ce53e17822a53a1445dc9752ed155e892a2
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2024-03-27 10:14:25 +0530

    Update Final.py

--- pages/Final.py
+++ pages/Final.py
@@ -5,5 +5,5 @@ def main():
   with st.spinner("Generating Head Line!!!"):
     st.success(Generate(st.session_state['Questions'][-1]))
 
-if __name__ == __main__:
+if __name__ == "__main__":
   main()
