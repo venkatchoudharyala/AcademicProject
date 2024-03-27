7170cff1568ca7bfc73af24ad8e6811e01cf4964
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2024-03-27 14:10:24 +0530

    Update App.py

--- App.py
+++ App.py
@@ -10,19 +10,6 @@ def main():
 	st.title("Abstract Page")
 	with st.spinner("Checking for necessary Dependencies"):
 		bl.Booting()
-	
-	k = os.listdir("Parser/stanford-corenlp-4.5.6")
-	flag = 0
-	for i in k:
-		if i == "stanford-corenlp-4.5.6-models.jar" or i == "stanford-corenlp-4.5.6-sources.jar":
-			flag += 1
-	if flag == 2:
-		st.success('Parser Files Found!', icon="✅")
-
-	st.session_state['server'] = CoreNLPServer()
-	with st.spinner("Initializing CoreNLP Server!"):
-		st.session_state['server'].start()
-		st.success('Server Initialized', icon="✅")
 
 	if st.button("Upload Text"):
 		st.switch_page("pages/Extraction.py")
