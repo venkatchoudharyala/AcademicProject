b19edef5737574d3a1e343760c69a56c0518e04e
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2024-03-27 09:18:28 +0530

    Update App.py

--- App.py
+++ App.py
@@ -4,6 +4,7 @@ from WebComponents import BootLoader as bl
 def main():
 	with st.spinner("Checking for necessary Dependencies"):
 		bl.Booting()
-	st.switch_page("pages/Extraction.py")
+	if st.button("Upload Text"):
+		st.switch_page("pages/Extraction.py")
 if __name__ == "__main__":
 	main()
