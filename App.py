65c33dd6999d6d990af9c5ffaebaad4b03e89ada
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2024-03-27 08:55:30 +0530

    Update App.py

--- App.py
+++ App.py
@@ -2,6 +2,8 @@ import streamlit as st
 from WebComponents import BootLoader as bl
 
 def main():
-	
+	with st.spinner("Checking for necessary Dependencies"):
+		bl.Booting()
+	st.switch_page("Pages/Extraction.py")
 if __name__ == "__main__":
 	main()
