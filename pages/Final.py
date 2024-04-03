a9f0efcad843beb07152c0bfcc7705e99c239247
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2024-04-03 20:59:54 +0530

    Update Final.py

--- pages/Final.py
+++ pages/Final.py
@@ -4,7 +4,7 @@ from CompleteSystemProto import Generate
 def main():
 	if 'Questions' in st.session_state:
 		with st.spinner("Generating Head Line!!!"):
-			st.subheader(st.session_state['Questions'][-1]))
+			st.subheader(st.session_state['Questions'][-1])
 			st.success(Generate(st.session_state['Questions'][-1]))
 	else:
 		st.error("Please Navigate back to Extraction Page and Upload your Article")
