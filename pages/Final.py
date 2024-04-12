d14ccd6648dcf84b053b1a7a49155f91c78a9639
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2024-04-12 10:29:47 +0530

    Update Final.py

--- pages/Final.py
+++ pages/Final.py
@@ -5,7 +5,7 @@ def main():
 	if 'Questions' in st.session_state:
 		with st.spinner("Generating Head Line!!!"):
 			st.subheader(st.session_state['Questions'][-1])
-			st.success(Generate(st.session_state['Questions'][-1]))
+			st.success(Generate(st.session_state['Questions'][-1], st.sessio_state["Server"]))
 	else:
 		st.error("Please Navigate back to Extraction Page and Upload your Article")
 
